abcd6c3a3d1a5f793c5f19243b3168cfebe71adc
Cristiam Herrera <cristiamherrera@gmail.com>
2020-03-25 15:16:33 -0400

    Se adiciono la importacion del excel

--- application/controllers/Excels.php
+++ application/controllers/Excels.php
@@ -1,5 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
+require_once FCPATH.'/application/libraries/SimpleXLSX.php';
 
 class Excels extends CI_Controller {
 
@@ -30,11 +31,64 @@ class Excels extends CI_Controller {
 
 	public function sube_excel()
 	{
+		$data['excels_subidos'] = $this->db->order_by('id', 'DESC')->get_where('excels', array('borrado' => NULL))->result();
+		// vdebug($excels_subidos, true, false, true);
 		$this->load->view('template/header');
 		$this->load->view('template/menu');
-		// $this->load->view('trabajos/nuevo', $data);
-		$this->load->view('excels/sube_excel');
+		$this->load->view('excels/sube_excel', $data);
 		$this->load->view('template/footer');
+
+	}
+
+	public function guarda_excel()
+	{
+		$usuario_id = $this->session->id_usuario;
+
+		$config['upload_path']   = './archivos/';
+		$config['allowed_types'] = 'xlsx';
+		$config['max_size']      = 5000;
+		
+		$this->load->library('upload', $config);
+		
+		if (!$this->upload->do_upload('archivo')) {
+			
+			$error = array('error' => $this->upload->display_errors());
+			
+			// $this->load->view('upload_form', $error);
+		} else {
+			$gestion = date('Y');
+			$hoy = date("Y-m-d H:i:s");
+			$data = array('upload_data' => $this->upload->data());
+			$datos_archivo = $this->upload->data();
+			$nombre_archivo = $this->upload->data('file_name');
+
+			$datos_excel = array(
+				'usuario_id'     => $usuario_id,
+				'nombre_archivo' => $nombre_archivo,
+				'mes'            => $this->input->post('mes'),
+				'gestion'        => $gestion,
+				'fecha'          => $hoy
+			);
+			$this->db->insert('excels', $datos_excel);
+
+			echo '<h1>Datos del Excel.xslx</h1><pre>';
+			if ($xlsx = SimpleXLSX::parse("./archivos/$nombre_archivo")) {
+				print_r($xlsx->rows());
+			} else {
+				echo SimpleXLSX::parseError();
+			}
+			echo '<pre>';
+
+			// redirect("excels/sube_excel");
+
+			// $id_cliente = $this->db->insert_id();
+
+			// vdebug($datos_archivo['file_name'], true, false, false);
+			// vdebug($this->upload->data(), true, false, false);
+			// echo 'si subio';
+
+			// $this->load->view('upload_success', $data);
+		}
 	}
 
 	public function index() 
@@ -42,40 +96,108 @@ class Excels extends CI_Controller {
 		$this->load->view('welcome_message');
 	}
 
-	public function listado()
+	public function prueba()
 	{
-		$data['aberturas'] = $this->db->get_where('aberturas', array('borrado ='=>NULL))->result_array();		
-		// echo 'Holas desde listado';
-		// vdebug($clientes, true, false, true);
-		$this->load->view('template/header');
-		$this->load->view('template/menu');
-		// $this->load->view('trabajos/nuevo', $data);
-		$this->load->view('aberturas/listado', $data);
-		$this->load->view('template/footer');
-	}
+		// echo '<h1>Parse books.xslx</h1><pre>';
+		if ($xlsx = SimpleXLSX::parse("./archivos/abril_2020.xlsx")) {
+			// print_r($xlsx->rows());
+			foreach ($xlsx->rows() as $key => $r) {
+				if($key > 0){
+					if ($key == 1 ) {
+						if ($r[3] == 'mañana') {
+							$datos_marcaciones = array(
+								'excel_id' => 1,
+								'carnet'   => $r[0],
+								'fecha'    => $r[2],
+								'man_hi'   => $r[6],
+								'man_hs'   => $r[7],
+							);
+						} else {
+							$datos_marcaciones = array(
+								'excel_id' => 1,
+								'carnet'   => $r[0],
+								'fecha'    => $r[2],
+								'tar_hi'   => $r[6],
+								'tar_hs'   => $r[7],
+							);
+						}
+						$this->db->insert('asistencias', $datos_marcaciones);
+					}else{
+						$consulta_persona  = $this->db->order_by('id', 'DESC')->get_where('asistencias', array('carnet' => $r[0], 'borrado =' => NULL, 'fecha'=>$r[2]))->row_array();
+						if ($consulta_persona) {
+							// vdebug($consulta_persona, true, false, true);
+							if ($r[3] == 'mañana') {
+								// echo $r[3];
+								$datos_marcaciones = array(
+									'excel_id' => 1,
+									'carnet'   => $r[0],
+									'fecha'    => $r[2],
+									'man_hi'   => $r[6],
+									'man_hs'   => $r[7],
+								);
+							} else {
+								$datos_marcaciones = array(
+									'excel_id' => 1,
+									'carnet'   => $r[0],
+									'fecha'    => $r[2],
+									'tar_hi'   => $r[6],
+									'tar_hs'   => $r[7],
+								);
+							}
+							$this->db->where('id', $consulta_persona['id']);
+							$this->db->update('asistencias', $datos_marcaciones);
+						}else{
+							if ($r[3] == 'mañana') {
+								$datos_marcaciones = array(
+									'excel_id' => 1,
+									'carnet'   => $r[0],
+									'fecha'    => $r[2],
+									'man_hi'   => $r[6],
+									'man_hs'   => $r[7],
+								);
+							} else {
+								$datos_marcaciones = array(
+									'excel_id' => 1,
+									'carnet'   => $r[0],
+									'fecha'    => $r[2],
+									'tar_hi'   => $r[6],
+									'tar_hs'   => $r[7],
+								);
+							}
+							$this->db->insert('asistencias', $datos_marcaciones);
+						}
+					}
 
-	public function guarda()
-	{
-		$id = $this->input->post('ida');
-		// vdebug($this->input->post('ida'), true, false, true);
-		$datos = array(
-			'nombre' => $this->input->post('nombre'),
-			'tipo'   => $this->input->post('tipo'),
-			'genero' => $this->input->post('genero'),
-		);
-		if (empty($id)) {
-			$this->db->insert('aberturas', $datos);
+				}
+			}
 		} else {
-			$this->db->where('id', $id);
-			$this->db->update('aberturas', $datos);
+			echo SimpleXLSX::parseError();
 		}
-		redirect("aberturas/listado");
-	}
+		echo '<pre>';
+		// output worsheet 1
+		// $xlsx = SimpleXLSX::parse("./archivos/abril_20206.xlsx");
 
-	public function eliminar($id_abertura = null)
-	{
-		$hoy = date("Y-m-d H:i:s");
-		$this->db->update('aberturas', array('borrado'=>$hoy), "id=$id_abertura");
-		redirect("aberturas/listado");
+		// $dim = $xlsx->dimension();
+		// $num_cols = $dim[0];
+		// $num_rows = $dim[1];
+
+		/*echo '<table border=1>';
+		foreach ($xlsx->rows() as $r) {
+			echo '<tr>';
+			for ($i = 0; $i < $num_cols; $i++) {
+				echo '<td>' . (!empty($r[$i]) ? $r[$i] : '&nbsp;') . '</td>';
+			}
+			echo '</tr>';
+		}
+		echo '</table>';*/
+
+		/*foreach ($xlsx->rows() as $r) {
+			for ($i = 0; $i < $num_cols; $i++) {
+				// echo '<td>' . (!empty($r[$i]) ? $r[$i] : '&nbsp;') . '</td>';
+				echo 'nombre: '.$r[$i]. '<br />';
+			}
+		}*/
+
 	}
+	
 }
--- application/views/excels/sube_excel.php
+++ application/views/excels/sube_excel.php
@@ -1,157 +1,134 @@
    <!-- ============================================================== -->
-    <!-- Page wrapper  -->
-    <!-- ============================================================== -->
-    <div class="page-wrapper">
-        <!-- ============================================================== -->
-        <!-- Container fluid  -->
-        <!-- ============================================================== -->
-        <div class="container-fluid">
-            <!-- ============================================================== -->
-            <!-- Start Page Content -->
-            <!-- ============================================================== -->
-            <div class="row">
+   <!-- Page wrapper  -->
+   <!-- ============================================================== -->
+   <div class="page-wrapper">
+       <!-- ============================================================== -->
+       <!-- Container fluid  -->
+       <!-- ============================================================== -->
+       <div class="container-fluid">
+           <!-- ============================================================== -->
+           <!-- Start Page Content -->
+           <!-- ============================================================== -->
+           <div class="row">
 
-                <div class="col-md-12">
-                    <div class="card card-outline-info">
-                        <div class="card-header">
-                            <h4 class="mb-0 text-white">REGISTRO DE PAGO</h4>
-                        </div>
+               <div class="col-md-12">
+                   <div class="card card-outline-info">
+                       <div class="card-header">
+                           <h4 class="mb-0 text-white">RETGISTRO DE ARCHIVOS EXCEL</h4>
+                       </div>
 
-                        <div class="card-body">
-                            <div class="row">
+                       <div class="card-body">
 
-                                <div class="col-md-12">
-                                    <table class="table table-bordered no-wrap">
-                                        <tr>
-                                            <td><span class="font-bold">Nombre: </span> <?php echo $trabajo['nombre'] ?></td>
-                                            <td><span class="font-bold">Celulares: </span> <?php echo $trabajo['celulares'] ?></td>
-                                            <td><span class="font-bold">F. Entrega: </span> <?php echo $trabajo['fecha_entrega'] ?></td>
-                                            <td><span class="font-bold">F. Prueba: </span> <?php echo $trabajo['fecha_prueba'] ?></td>
-                                            <td><span class="font-bold">Entregado: </span> <?php echo $trabajo['entregado'] ?></td>
-                                        </tr>
-                                    </table>
-                                </div>
-                            </div>
-                            <div class="row">
+                           <div class="row">
 
-                                <div class="col-md-3">
-                                    <!-- <form action="<?php //echo base_url() ?>Trabajos/guarda_pago" method="POST"> -->
-                                    <?php echo form_open('trabajos/guarda_pago'); ?>
-                                        <div class="row">
+                               <div class="col-md-3">
+                                   <?php echo form_open_multipart('excels/guarda_excel'); ?>
+                                   <div class="row">
 
-                                            <div class="col-md-5">
-                                                <div class="form-group">
-                                                    <label class="control-label">Monto </label>
-                                                    <input type="hidden" name="trabajo_id" value="<?php echo $trabajo['id'] ?>">
-                                                    <input type="hidden" name="cliente_id" value="<?php echo $trabajo['cliente_id'] ?>">
-                                                    <input type="number" name="monto" id="monto" class="form-control" placeholder="Ej: 200" step="any" max="<?php echo $trabajo['saldo']; ?>" autofocus required>
-                                                </div>
-                                            </div>
+                                       <div class="col-md-12">
+                                           <div class="form-group">
+                                               <label class="control-label">Archivo (Nombre:<span style="color: red;"> enero_2020.xlsx</span>)</label>
+                                               <input type="file" name="archivo" class="form-control" required>
+                                           </div>
+                                       </div>
+                                   </div>
+                                   <div class="row">
 
-                                            <div class="col-md-7">
-                                                <div class="form-group">
-                                                    <label class="control-label">Fecha </label>
-                                                    <input type="date" name="fecha" id="fecha" class="form-control" value="<?php echo date('Y-m-d');?>">
-                                                </div>
-                                            </div>
+                                       <div class="col-md-12">
+                                           <div class="form-group">
+                                               <label class="control-label">Mes</label>
+                                               <select name="mes" id="mes" class="form-control custom-select">
+                                                   <option value="Enero">Enero</option>
+                                                   <option value="Febrero">Febrero</option>
+                                                   <option value="Marzo">Marzo</option>
+                                                   <option value="Abril">Abril</option>
+                                                   <option value="Mayo">Mayo</option>
+                                                   <option value="Junio">Junio</option>
+                                                   <option value="Julio">Julio</option>
+                                                   <option value="Agosto">Agosto</option>
+                                                   <option value="Septiembre">Septiembre</option>
+                                                   <option value="Octubre">Octubre</option>
+                                                   <option value="Noviembre">Noviembre</option>
+                                                   <option value="Diciembre">Diciembre</option>
+                                               </select>
+                                           </div>
+                                       </div>
 
-                                        </div>
+                                   </div>
 
-                                        <div class="custom-control custom-checkbox mr-sm-2 mb-3">
-                                            <input type="checkbox" class="custom-control-input" id="checkbox0" name="entregado" value="si">
-                                            <label class="custom-control-label" for="checkbox0">Entregar trabajo</label>
-                                        </div>
+                                   <div class="row">
+                                       <div class="col-md-12">
+                                           <button type="submit" class="btn waves-effect waves-light btn-block btn-success">GUARDA PAGO</button>
+                                       </div>
+                                   </div>
+                                   </form>
+                               </div>
 
-                                        <div class="row">
-                                            <div class="col-md-12">
-                                                <button type="submit" class="btn waves-effect waves-light btn-block btn-success">GUARDA PAGO</button>
-                                                <p></p>
-                                                <a 
-                                                    href="<?php echo base_url() ?>trabajos/impresion_recibo/<?php echo $trabajo['id']; ?>"
-                                                    class="btn waves-effect waves-light btn-block btn-info"
-                                                >
-                                                    IMPRIME RECIBO
-                                                </a>
-                                            </div>
-                                        </div>
-                                    </form>
-                                </div>
+                               <div class="col-md-9">
+                                   <div class="card card-outline-primary">
+                                       <div class="card-header">
+                                           <h4 class="mb-0 text-white">LISTADO DE ARCHIVOS</h4>
+                                       </div>
+                                       <?php //vdebug($eagos, false, false, true) 
+                                        ?>
+                                       <?php $total = 0; ?>
+                                       <table class="table table-striped no-wrap">
+                                           <thead>
+                                               <tr>
+                                                   <th>Excel</th>
+                                                   <th>Fecha</th>
+                                                   <th style="width: 5%;">Acciones</th>
+                                               </tr>
+                                           </thead>
+                                           <tbody>
+                                               <?php foreach ($excels_subidos as $e) : ?>
+                                                   <tr>
+                                                       <td><?php echo $e->nombre_archivo; ?></td>
+                                                       <td><?php echo fechaEs($e->fecha); ?></td>
+                                                       <td></td>
+                                                       <td align="left">
+                                                           <button type="button" class="btn btn-danger"><i class="fas fa-times"></i></button>
+                                                       </td>
+                                                   </tr>
+                                               <?php endforeach; ?>
+                                           </tbody>
+                                       </table>
+                                   </div>
 
-                                <div class="col-md-9">
-                                    <div class="card card-outline-primary">                                
-                                    <div class="card-header">
-                                        <h4 class="mb-0 text-white">LISTADO DE PAGOS</h4>
-                                    </div>
-                                    <?php //vdebug($pagos, false, false, true) ?>
-                                    <?php $total=0; ?>
-                                        <table class="table table-striped no-wrap">
-                                            <thead>
-                                                <tr>
-                                                    <th>Fecha</th>
-                                                    <th>Usuario</th>
-                                                    <th>Monto</th>
-                                                    <th style="width: 5%;"></th>
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-                                                <?php foreach ($pagos as $p): ?>
-                                                <?php $total += $p['monto']; ?>
-                                                    <tr>
-                                                        <td><?php echo fechaEs($p['fecha']); ?></td>
-                                                        <td><?php echo $p['nombre']; ?></td>
-                                                        <td><?php echo $p['monto']; ?></td>
-                                                        <td></td>
-                                                        <td align="left">
-                                                            <button type="button" class="btn btn-danger" onclick="alerta(<?php echo $p['id'] ?>, <?php echo $p['monto'] ?>, <?php echo $p['trabajo_id'] ?>)"><i class="fas fa-times"></i></button>
-                                                        </td>
-                                                    </tr>
-                                                <?php endforeach; ?>
-                                            </tbody>
-                                            <tfoot>
-                                                <tr>
-                                                    <th class="text-info">Costo Total: <?php echo $trabajo['total'] ?></th>
-                                                    <th align="right"></th>
-                                                    <th><?php echo $total; ?></th>
-                                                    <th><span class="text-danger">Saldo: <?php echo $trabajo['saldo']; ?></span></th>
-                                                </tr>
-                                            </tfoot>
-                                        </table>
-                                    </div>
-
-                                    </div>
+                               </div>
 
 
-                            </div>
+                           </div>
 
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+                       </div>
+                   </div>
+               </div>
+           </div>
+       </div>
+   </div>
 
-<script type="text/javascript">
-    function alerta(id_pago, monto, id_trabajo){
-        //console.log(id_pago);
-        Swal.fire({
-          title: 'Quieres borrar '+monto+'?',
-          text: "Luego no podras recuperarlo!",
-          type: 'warning',
-          showCancelButton: true,
-          confirmButtonColor: '#3085d6',
-          cancelButtonColor: '#d33',
-          confirmButtonText: 'Si, estoy seguro!',
-          cancelButtonText: "Cancelar",
-        }).then((result) => {
-          if (result.value) {
-            Swal.fire(
-              'Excelente!',
-              'Tu monto fue borrado.',
-              'success'
-            );
-            // console.log("el id es "+id_pago);
-            window.location.href = "<?php echo base_url() ?>trabajos/borra_pago/"+id_pago+"/"+id_trabajo;
-          }
-        })
-    }
-</script>
+   <script type="text/javascript">
+       function alerta(id_pago, monto, id_trabajo) {
+           //console.log(id_pago);
+           Swal.fire({
+               title: 'Quieres borrar ' + monto + '?',
+               text: "Luego no podras recuperarlo!",
+               type: 'warning',
+               showCancelButton: true,
+               confirmButtonColor: '#3085d6',
+               cancelButtonColor: '#d33',
+               confirmButtonText: 'Si, estoy seguro!',
+               cancelButtonText: "Cancelar",
+           }).then((result) => {
+               if (result.value) {
+                   Swal.fire(
+                       'Excelente!',
+                       'Tu monto fue borrado.',
+                       'success'
+                   );
+                   // console.log("el id es "+id_pago);
+                   window.location.href = "<?php echo base_url() ?>trabajos/borra_pago/" + id_pago + "/" + id_trabajo;
+               }
+           })
+       }
+   </script>
