9479d5a4453ed0fa53ad254d3533f55fad5769e5
cristiam <cristiamherrera@gmail.com>
2020-07-07 18:52:06 -0400

    prueba 3

--- application/libraries/Pdf.php
+++ application/libraries/Pdf.php
@@ -1,11 +1,6 @@
-
-
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 // Dompdf namespace
 use Dompdf\Dompdf;
-
-
-
 class Pdf{
 	public function __construct(){
 		require_once dirname(__FILE__).'/dompdf/autoload.inc.php';
