8562a5e1310085761830564ec6f4b93fe760d065
Joel Flores <jjjoelcito@gmail.com>
2022-01-12 15:21:28 -0400

    SE agrego el cambio ed tablas y la agreagacion de nuevo proidcuto

--- application/controllers/Movimiento.php
+++ application/controllers/Movimiento.php
@@ -13,19 +13,10 @@ class Movimiento extends CI_Controller {
 
     public function sacaProducto(){
 
-        $datos = array(
-			'categoria_id'  => $this->input->post('categoria_id'),
-			'cantidad'      => $this->input->post('cantidad-sacado'),
-            'fecha'         => date("Y-m-d"),
-			'detalle'       => $this->input->post('detalle-quitar'),
-			'estado'        => 1
-		);
-		$this->db->insert('ventas', $datos);
-
         $user_id = $this->session->id_usuario;
         $datos1 = array(
             'usuarios_id'    => $user_id,
-            'categoria_id'  => $this->input->post('categoria_id'),
+            'producto_id'  => $this->input->post('producto_id'),
             'almacen_id'    => $this->session->almacen_id,
 			'salida'        => $this->input->post('cantidad-sacado'),
             'fecha'         => date("Y-m-d"),
@@ -39,29 +30,40 @@ class Movimiento extends CI_Controller {
 
 	public function agregarProducto(){
 
-        $datos = array(
-			'categoria_id'  => $this->input->post('categoria_id-agregar'),
-			'stock'      	=> $this->input->post('cantidad-agregar'),
-            'fecha'         => date("Y-m-d"),
-			'detalle'       => $this->input->post('detalle-agregar'),
-			'estado'        => 1
-		);
-		$this->db->insert('compras', $datos);
-
-
         $user_id = $this->session->id_usuario;
         $datos1 = array(
             'usuarios_id'    => $user_id,
-            'categoria_id'  => $this->input->post('categoria_id-agregar'),
+            'producto_id'  => $this->input->post('producto_id-agregar'),
             'almacen_id'    => $this->session->almacen_id,
 			'ingreso'        => $this->input->post('cantidad-agregar'),
             'fecha'         => date("Y-m-d"),
 			'estado'        => 'Regularizacion',
 			'descripcion'   => $this->input->post('detalle-agregar'),
 		);
+
 		$this->db->insert('movimientos', $datos1);
 
         redirect ("Inventarios_Compra/lista");
     }
+
+	public function agregarProductoLegal(){
+		$user_id = $this->session->id_usuario;
+        $datos1 = array(
+            'usuarios_id'    => $user_id,
+            'producto_id'    => $this->input->post('producto_id'),
+            'almacen_id'     => $this->session->almacen_id,
+			'precio_compra'  =>	$this->input->post('precio_unidad'),
+			'precio_venta'   => $this->input->post('precio_venta'),
+			'precio_total'   => $this->input->post('precio_total'),
+			'ingreso'		 => $this->input->post('stock'),
+            'fecha'          => $this->input->post('fecha'),
+			'descripcion'    => $this->input->post('detalle'),
+			'created_at'	 => date("Y-m-d H:i:s"),
+		);
+
+		$this->db->insert('movimientos', $datos1);
+
+        redirect ("Inventarios_Compra/lista");
+	}
 
 }
